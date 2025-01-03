91947fd11001f8b7f545bd19ab0c93bce553faf7
mileprogramer <obecano.ispunjeno@gmail.com>
2025-01-03 14:59:06 +0100

    fix errors on npm run build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import fs from 'fs';
+import path from 'path';
+
+const tsFiles = fs
+    .readdirSync(path.resolve(__dirname, 'resources/ts/pages'))
+    .filter(file => file.endsWith('.ts'))
+    .map(file => `resources/ts/pages/${file}`);
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/ts/app.ts'],
+            input: [
+                'resources/css/app.css',
+                'resources/ts/app.ts',
+                ...tsFiles
+            ],
             refresh: true,
         }),
     ],
